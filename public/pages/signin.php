8e69ab8f9e37683969cb31c704b2bf3fe7e5aefb
jahidulsaeid <jahidul.saeid@gmail.com>
2021-03-08 11:52:28 +0600

    add form

---
+++ public/pages/signin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Resturent Booking -  Sing in</title>
+</head>
+
+<body>
+
+    <form action="../inc/signin.php" method="post">
+
+        <input type="text" name="name" id="">
+        <input type="text" name="phone" id="">
+        <select name="number_of_person" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+        </select>
+
+    </form>
+
+</body>
+
+</html>
